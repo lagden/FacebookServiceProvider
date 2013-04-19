af2ab2e58d24c4012b0669f2eb3ab8a08514ec61
Thiago Lagden <lagden@gmail.com>
2013-04-19 18:35:31 -0300

    more simple

--- src/Lagden/Silex/FacebookServiceProvider.php
+++ src/Lagden/Silex/FacebookServiceProvider.php
@@ -6,22 +6,12 @@ use Silex\ServiceProviderInterface;
 
 class FacebookServiceProvider implements ServiceProviderInterface
 {
-    private $appId;
-    private $appSecret;
-
-    public function __construct($options)
-    {
-        $this->appId     = $options['facebook.app_id'];
-        $this->appSecret = $options['facebook.app_secret'];
-    }
-
     public function register(Application $app)
     {
-
-        $app['facebook'] = $app->share(function ($app) {
+        $app['facebook'] = $app->share(function () use ($app) {
             return new \Facebook(array(
-                'appId'  => $this->appId,
-                'secret' => $this->appSecret,
+                'appId'  => $app['facebook.app_id'];,
+                'secret' => $app['facebook.app_secret'];,
             ));
         });
     }
