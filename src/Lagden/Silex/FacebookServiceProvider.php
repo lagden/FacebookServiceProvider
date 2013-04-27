45c0da619e10c27319addcc5b46ed7e15371589a
Thiago Lagden <lagden@gmail.com>
2013-04-27 01:34:38 -0300

    more data

--- src/Lagden/Silex/FacebookServiceProvider.php
+++ src/Lagden/Silex/FacebookServiceProvider.php
@@ -13,6 +13,8 @@ class FacebookServiceProvider implements ServiceProviderInterface
                 'appId'  => $app['facebook.app_id'],
                 'secret' => $app['facebook.app_secret'],
                 'fileUpload' => $app['facebook.file_upload'],
+                'sharedSession' => $app['facebook.sharedSession'],
+                'trustForwarded' => $app['facebook.trustForwarded'],
             ));
         });
     }
